f88090acc9048a217c2cdfa9c4a6cd3d88a99920
Michalina Chęć <michalinachec@gmail.com>
2024-05-27 12:43:07 +0200

    added before to desktop and login

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -1,43 +1,39 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('desktop test', () => {
-    test('quick payment with correct data', async ({ page }) => {
+
+    test.beforeEach(async ({ page }) => {
         const url = 'https://demo-bank.vercel.app/';
         const userID = 'testerLO';
         const userPassword = '10425689';
-        const recevierID = '2';
-        const transferAmount = '120';
-        const transferTitle = 'zwrot srodkow';
-        const expectedTransferReceiver = 'Chuck Demobankowy';
 
         await page.goto(url);
         await page.getByTestId('login-input').fill(userID);
         await page.getByTestId('password-input').fill(userPassword);
         await page.getByTestId('login-button').click();
-       
+    });
+
+    test('quick payment with correct data', async ({ page }) => {
+        const recevierID = '2';
+        const transferAmount = '120';
+        const transferTitle = 'zwrot srodkow';
+        const expectedTransferReceiver = 'Chuck Demobankowy';
+
+
         await page.locator('#widget_1_transfer_receiver').selectOption(recevierID);
         await page.locator('#widget_1_transfer_amount').fill(transferAmount);
         await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
         await page.getByRole('button', { name: 'wykonaj' }).click();
         await page.getByTestId('close-button').click();
-         
+
         await expect(page.locator('#show_messages')).toHaveText(`Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`);
     });
-    
+
     test('successful mobile top-up', async ({ page }) => {
-        const url = 'https://demo-bank.vercel.app/';
-        const userID = 'testerLO';
-        const userPassword = '10425689';
         const topUpReceiver = '500 xxx xxx';
         const topUpAmount = '50';
         const expectedMessage = `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${topUpReceiver}`;
-
-    
-        await page.goto(url);
-        await page.getByTestId('login-input').fill(userID);
-        await page.getByTestId('password-input').fill(userPassword);
-        await page.getByTestId('login-button').click();
 
         await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
         await page.locator('#widget_1_topup_amount').fill(topUpAmount);
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('successful login with correct credentials', () => {
-  test('login with correct credentials', async ({ page }) => {
-    const url = 'https://demo-bank.vercel.app/';
+test.describe('user login do Demobank', () => {
+
+    test.beforeEach(async ({page}) => {
+        const url = 'https://demo-bank.vercel.app/';
+        await page.goto(url);
+    });
+
+  test('successful login with correct credentials', async ({ page }) => {
+   
     const userID = 'testerLO';
     const userPassword = '10425689';
     const expectedUserName = 'Jan Demobankowy';
 
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userID);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
@@ -16,24 +21,22 @@ test.describe('successful login with correct credentials', () => {
   });
 
   test('unsuccessful login with too short username', async ({ page }) => {
-    const url = 'https://demo-bank.vercel.app/';
+  
     const incorrectUserId = 'tester';
     const expectedErrorMessage = 'identyfikator ma min. 8 znaków'; 
 
-    await page.goto(url);
     await page.getByTestId('login-input').fill(incorrectUserId);
     await page.getByTestId('password-input').click();
 
     await expect(page.getByTestId('error-login-id')).toHaveText(expectedErrorMessage);
   });
 
-  test('unsuccessful login with to short password', async ({ page }) => {
-    const url = 'https://demo-bank.vercel.app/';
+  test('unsuccessful login with too short password', async ({ page }) => {
+    
     const userID = 'testerLO';
     const incorretcUserPassword = '104';
     const expectedErrorMessage = 'hasło ma min. 8 znaków';
 
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userID);
     await page.getByTestId('password-input').fill(incorretcUserPassword);
     await page.getByTestId('password-input').blur();
